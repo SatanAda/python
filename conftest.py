3ac14302011a954d04215d401e1f0c875d3c31e0
SatanAda <angelada@inbox.ru>
2021-08-10 12:16:42 +0300

    Add files via upload

--- conftest.py
+++ conftest.py
@@ -1,33 +1,34 @@
+import datetime
+
 import pytest
 
+from dotenv import load_dotenv
 from selenium import webdriver
-from funkcii.imap import imap
-from funkcii.aktivator import SMSActivator
+
+from utils.imap import imap
+from utils.twillio import SmsFromTwillio
 
 
-@pytest.fixture (autouse=True)
+@pytest.fixture(scope='session')
 def driver():
-    driver = webdriver.Chrome ()
-    driver.maximize_window ()
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
     return driver
 
 
-@pytest.fixture (scope='class')
-def auth():
-    mail = imap ()
-    link = mail
-    return link
+@pytest.fixture(scope='session')
+def get_link():
+    return imap()
 
 
-@pytest.fixture (scope='session')
-def get_number():
-    activator = SMSActivator ()
-    phone_number = activator.get_number ()
-    return phone_number,
+@pytest.fixture(autouse=True)
+def dotenv():
+    return load_dotenv()
 
 
-@pytest.fixture (scope='session')
-def wait_sms():
-    activator = SMSActivator ()
-    code = activator.wait_sms ()
-    return code
+@pytest.fixture(scope='session')
+def get_sms():
+    return SmsFromTwillio()
